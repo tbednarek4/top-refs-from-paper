a8d86203c8b77b0f6f82c7faa81d0340a763a305
Tomasz Bednarek <40144687+tbednarek4@users.noreply.github.com>
2023-02-15 12:37:00 +0100

    Cleaned output titles from HTML tags

--- script.py
+++ script.py
@@ -1,33 +1,22 @@
-# top-refs-from-paper: simple Python script to retrieve the citations from Crossref API
-# Copyright (C) 2023 Tomasz Bednarek
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published
-# by the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-import requests
+import re, requests
+from bs4 import BeautifulSoup
 from collections import Counter
 from prettytable import PrettyTable
-from bs4 import BeautifulSoup
 
-def download_pdf(doi_link):
+CLEANR = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')\
+
+def cleanhtml(raw_html):
+  cleantext = re.sub(CLEANR, '', raw_html)
+  return cleantext
+
+def get_pdf(doi_link, filename):
   response = requests.get(f'https://sci-hub.ru/{doi_link}')
   soup = BeautifulSoup(response.content, 'lxml')
   papers = soup.find_all('embed')[0].get('src').rsplit('#', 1)[0]
-
-  with open('temp.pdf', 'wb') as f:
+  
+  with open(filename, 'wb') as f:
       f.write(requests.get(f'https://sci-hub.ru/{papers}').content)
       return True
-    
   return False
 
 def get_references(doi_link):
@@ -39,9 +28,9 @@ def get_references(doi_link):
     return ref_list
   else: pass
 
-def get_titles(doi_link):
+def get_title(doi_link):
   response = requests.get(f'https://api.crossref.org/works/{doi_link}')
-  output = response.json().get('message', {}).get('title')
+  output = cleanhtml(response.json().get('message', {}).get('title')[0])
   return output
 
 def get_most_popular_refs(first_link):
@@ -51,7 +40,7 @@ def get_most_popular_refs(first_link):
   res = Counter(second_list).most_common()
 
   my_table = PrettyTable()
-  my_table.field_names = ["Count", "Title", "DOI"]
+  my_table.field_names = ["Count", "DOI", "Title"]
 
   for doi, count in res[:50]:
     title = get_titles(doi)
@@ -63,5 +52,5 @@ def get_most_popular_refs(first_link):
 if __name__ == '__main__':
   test_link_01 = '10.1002/anie.202013801'
   test_link_02 = '10.1021/acscatal.8b02371'
-  get_most_popular_refs(test_link_01) 
+  # get_most_popular_refs(test_link_01) 
   get_most_popular_refs(test_link_02)
